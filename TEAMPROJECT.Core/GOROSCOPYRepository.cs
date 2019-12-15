35f24ee58488a051b31329b455470349f7d8025b
Анна Савина <sav.ahha321@gmail.com>
2019-12-15 21:54:15 +0300

    serializer

---
+++ TEAMPROJECT.Core/GOROSCOPYRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace TEAMPROJECT.Core
+{
+    class GOROSCOPYRepository: IRepository
+    {
+        public virtual T Deserialize<T>(string fileName) {
+            using(var sr = new StreamReader(fileName)) {
+                using(var jsonReader = new JsonTextReader(sr)) 
+                {
+                    var serializer = new JsonSerializer();
+                    return serializer.Deserialize<T>(jsonReader);
+                }
+            }
+        }
+
+        public virtual void Serialize<T>(string fileName, T data)
+        {
+            using (var sw = new StreamWriter(fileName))
+            {
+                using (var jsonWriter = new JsonTextWriter(sw))
+                {
+                    var serializer = new JsonSerializer();
+                    serializer.Serialize(jsonWriter, data);
+                }
+            }
+        }
+    }
+}
--- TEAMPROJECT.Core/IRepository.cs
+++ TEAMPROJECT.Core/IRepository.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace TEAMPROJECT.Core
 {
     interface IRepository
     {
         //Прописать код для Json файлов
+        T Deserialize<T>(string fileName);
+        void Serialize<T>(string fileName, T data);      
     }
 }
