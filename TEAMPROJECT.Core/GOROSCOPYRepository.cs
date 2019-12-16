3ecf081c4eca38eb87c035594f2bcae4fa9b454d
Анна Савина <sav.ahha321@gmail.com>
2019-12-16 09:09:19 +0300

    new repository

--- TEAMPROJECT.Core/GOROSCOPYRepository.cs
+++ TEAMPROJECT.Core/GOROSCOPYRepository.cs
@@ -12,34 +12,8 @@ using TEAMPROJECT.Core.Interfaces;
 
 namespace TEAMPROJECT.Core
 {
-    class GOROSCOPYRepository: IRepository
+    class GOROSCOPYRepository: Repository
     {
-        //Регулярное вырожение для двух проектов
-        private Regex regex = new Regex(@"\\TEAMPROJECT\.GUI\.User\\bin\\Debug|\\TEAMPROJECT\.GUI\.Owner\\bin\\Debug");
-        public List<User> Users = new List<User>();
         
-        public virtual T Deserialize<T>(string fileName) {
-            using(var sr = new StreamReader(regex.Replace(Environment.CurrentDirectory, "") + fileName)) 
-            {
-                using(var jsonReader = new JsonTextReader(sr)) 
-                {
-                    var serializer = new JsonSerializer();
-                    return serializer.Deserialize<T>(jsonReader);
-                }
-            }
-        }
-        public virtual void Serialize<T>(string fileName, T data)
-        {
-            using (var sw = new StreamWriter(regex.Replace(Environment.CurrentDirectory, "") + fileName))
-            {
-                using (var jsonWriter = new JsonTextWriter(sw))
-                {
-                    var serializer = new JsonSerializer();
-                    serializer.Serialize(jsonWriter, data);
-                }
-            }
-        }
-
-         
     }
 }
