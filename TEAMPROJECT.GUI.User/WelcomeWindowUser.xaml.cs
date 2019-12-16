92bf9dc708f959b80d69d5e72b5ef05d39942582
Анна Савина <sav.ahha321@gmail.com>
2019-12-16 15:28:52 +0300

    куфвн

---
+++ TEAMPROJECT.GUI.User/WelcomeWindowUser.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TEAMPROJECT.GUI.User
+{
+    /// <summary>
+    /// Логика взаимодействия для WelcomeWindowUser.xaml
+    /// </summary>
+    public partial class WelcomeWindowUser : Window
+    {
+        public WelcomeWindowUser()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
