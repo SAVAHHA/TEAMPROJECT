980cfbaa94764e88b375044f92cccd29b6032fd3
Анна Савина <sav.ahha321@gmail.com>
2019-12-17 10:48:29 +0300

    + some methods in GOROSCOPY

--- TEAMPROJECT.Core/GOROSCOPYRepository.cs
+++ TEAMPROJECT.Core/GOROSCOPYRepository.cs
@@ -17,6 +17,28 @@ namespace TEAMPROJECT.Core
         List<Zodiac> Zodiacs = new List<Zodiac>();
         List<Prediction> Predictions = new List<Prediction>();
 
+        public int GetUserID_by_Login(string login, string password)
+        {
+            int Id = 0;
+            foreach (var user in Users)
+            {
+                if (user.Login == login && user.Password == password)
+                {
+                    Id = user.UserID;
+                }
+            }
+            return Id;
+        }
 
+        public void AddUser(string name, DateTime dateOfBirth, string login, string password)
+        {
+            User user = new User();
+            user.Name = name;
+            user.DateOfBirth = dateOfBirth;
+            user.Login = login;
+            user.Password = password;
+            user.UserID = Users.Count() + 1;
+            Users.Add(user);
+        }
     }
 }
