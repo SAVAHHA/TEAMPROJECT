e22dfa09ce8857700945f5e1b6e475c606284259
UUULYA <labkovich00@gmail.com>
2019-12-17 14:02:24 +0300

    A little bit

--- TEAMPROJECT.Core/Models/User.cs
+++ TEAMPROJECT.Core/Models/User.cs
@@ -10,6 +10,7 @@ namespace TEAMPROJECT.Core.Models
     {
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
+        public string Zodiac { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         public int UserID { get; set; }
