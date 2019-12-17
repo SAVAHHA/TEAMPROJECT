548f012bb54ebe08aaeb05182248acf5555c85d0
ElenaGreen <lena.grinkevich@mail.ru>
2019-12-18 00:32:41 +0300

    night commit

---
+++ TEAMPROJECT.Core/Models/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEAMPROJECT.Core.Models
+{
+    public class Test
+    {
+        public int TestId { get; set; }
+        public string Question { get; set; }
+        public int QuestionId { get; set; }
+        public int Result1 { get; set; }
+        public int Result2 { get; set; }
+        public int Result3 { get; set; }
+        public int Result4 { get; set; }
+        public int Result5 { get; set; }
+        public int Result6 { get; set; }
+
+    }
+}
