a318ffab047939a59f628ad2f4e4b45d1351c2cf
Анна Савина <sav.ahha321@gmail.com>
2019-12-19 14:41:07 +0300

    for Lena<3

--- TEAMPROJECT.GUI.User/RegistrationWindow.xaml.cs
+++ TEAMPROJECT.GUI.User/RegistrationWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TEAMPROJECT.Core;
 
 namespace TEAMPROJECT.GUI.User
 {
@@ -19,9 +20,24 @@ namespace TEAMPROJECT.GUI.User
     /// </summary>
     public partial class RegistrationWindow : Window
     {
+        GOROSCOPYRepository gOROSCOPYRepository = new GOROSCOPYRepository();
+        Repository repository = new Repository();
+
         public RegistrationWindow()
         {
             InitializeComponent();
+        }
+
+        private void RegistrationCancel_Click(object sender, RoutedEventArgs e)
+        {
+            WelcomeWindowUser welcomeWindow = new WelcomeWindowUser();
+            welcomeWindow.Show();
+            this.Close();
+        }
+
+        private void RegistrationOK_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
