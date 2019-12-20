be262f12bdc5b0230c68dd365a3ecaabfb0bc8a8
UUULYA <labkovich00@gmail.com>
2019-12-20 15:57:54 +0300

    woooooow

--- TEAMPROJECT.Core/Repository.cs
+++ TEAMPROJECT.Core/Repository.cs
@@ -17,9 +17,14 @@ namespace TEAMPROJECT.Core
         public Regex regex = new Regex(@"\\TEAMPROJECT\.GUI\.UserTestHoroscop\\bin|\\TEAMPROJECT\.GUI\.Owner\\bin");
         public List<User> Users = new List<User>();
         public List<Zodiac> Zodiacs = new List<Zodiac>();
-        
+
+        public Repository()
+        {
+            LoadData();
+        }
         public void LoadData()
         {
+            Zodiacs = Deserialize<List<Zodiac>>("../../TEAMPROJECT.Core/Data/Zodiacs.json");
             //Users = Deserialize<List<User>>("Users.json");
             //Zodiacs = Deserialize<List<Zodiac>>("Zodiacs.json");
         }
